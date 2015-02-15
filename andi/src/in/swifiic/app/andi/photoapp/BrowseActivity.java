cd539527c6a0b183b928a8e0dac82cec3aae4389
Aniket Garg <aniketgarg@hotmail.com>
2015-02-15 12:49:16 +0530

    Images taken from camera are now saved in extFilesDir

--- andi/src/in/swifiic/app/andi/photoapp/BrowseActivity.java
+++ andi/src/in/swifiic/app/andi/photoapp/BrowseActivity.java
@@ -1,8 +1,16 @@
 package in.swifiic.app.andi.photoapp;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
+@SuppressLint("SimpleDateFormat")
 public class BrowseActivity extends ListActivity {
 
 	ListView list;
@@ -48,10 +57,7 @@ public class BrowseActivity extends ListActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		int itemId = item.getItemId();
 		if (itemId == R.id.action_shoot) {
-			Intent cameraIntent = new Intent(
-					android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-			startActivityForResult(cameraIntent, 1337);
-			// TODO Get image from camera and create a bundle from it
+			dispatchTakePictureIntent();
 			return true;
 		} else if (itemId == R.id.action_settings) {
 			Intent selectedSettings = new Intent(this, SettingsActivity.class);
@@ -60,6 +66,48 @@ public class BrowseActivity extends ListActivity {
 		} else {
 			return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	String mCurrentPhotoPath;
+
+	private File createImageFile() throws IOException {
+	    // Create an image file name
+	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+	    String imageFileName = "JPEG_" + timeStamp + "_";
+	    File storageDir = getExternalFilesDir(null);
+//	            Environment.DIRECTORY_PICTURES);
+	    File image = File.createTempFile(
+	        imageFileName,  /* prefix */
+	        ".jpg",         /* suffix */
+	        storageDir      /* directory */
+	    );
+
+	    // Save a file: path for use with ACTION_VIEW intents
+	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+	    return image;
+	}
+	
+	static final int REQUEST_TAKE_PHOTO = 1;
+
+	private void dispatchTakePictureIntent() {
+	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	    // Ensure that there's a camera activity to handle the intent
+	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+	        // Create the File where the photo should go
+	        File photoFile = null;
+	        try {
+	            photoFile = createImageFile();
+	        } catch (IOException ex) {
+	            // Error occurred while creating the File
+	        	ex.printStackTrace();
+	        }
+	        // Continue only if the File was successfully created
+	        if (photoFile != null) {
+	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+	                    Uri.fromFile(photoFile));
+	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+	        }
+	    }
 	}
 }
 
--- andi/src/in/swifiic/app/andi/photoapp/SettingsActivity.java
+++ andi/src/in/swifiic/app/andi/photoapp/SettingsActivity.java
@@ -2,7 +2,6 @@ package in.swifiic.app.andi.photoapp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
@@ -25,10 +24,6 @@ public class SettingsActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
-//		int id = item.getItemId();
-//		if (id == R.id.action_settings) {
-//			return true;
-//		}
 		return super.onOptionsItemSelected(item);
 	}
 }
