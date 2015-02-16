5af8d480676c4aa5d541ad51c3fcf98fa55a94a0
Aniket Garg <aniketgarg@hotmail.com>
2015-02-16 14:55:51 +0530

    Successfully compressing and storing image for upload

--- andi/src/in/swifiic/app/andi/photoapp/BrowseActivity.java
+++ andi/src/in/swifiic/app/andi/photoapp/BrowseActivity.java
@@ -1,6 +1,7 @@
 package in.swifiic.app.andi.photoapp;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -8,6 +9,8 @@ import java.util.Date;
 import android.annotation.SuppressLint;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -67,72 +70,102 @@ public class BrowseActivity extends ListActivity {
 			return super.onOptionsItemSelected(item);
 		}
 	}
-	
+
+	/* -----------Logic for taking and processing image for upload----------- */
 	String mCurrentPhotoPath;
+	static final int REQUEST_TAKE_PHOTO = 100;
 
-	private File createImageFile() throws IOException {
+	private File createImageFile(boolean temp) throws IOException {
 		File storageDir = getExternalFilesDir(null);
-		// Create uploads Directory
-		File uploadDir = new File(storageDir.getAbsolutePath() + "/uploads/");
-		if(!uploadDir.isDirectory()) {
-			uploadDir.mkdir();
+		// Create temp and uploads Directories
+		File uploadDir;
+		if (temp)
+			uploadDir = new File(storageDir.getAbsolutePath() + "/temp/");
+		else
+			uploadDir = new File(storageDir.getAbsolutePath()
+					+ "/uploads/");
+		if (!uploadDir.isDirectory()) {
+			uploadDir.mkdirs();
 		}
-	    // Create an image file name
-	    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-	    String imageFileName = "JPEG_" + timeStamp + "_";
-	    File image = File.createTempFile(
-	        imageFileName,  /* prefix */
-	        ".jpg",         /* suffix */
-	        uploadDir      /* directory */
-	    );
-
-	    // Save a file: path for use with ACTION_VIEW intents
-	    mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-	    return image;
+		// Create an image file name
+		String timeStamp = new SimpleDateFormat("yyMMdd_HHMMSS")
+				.format(new Date());
+		String imageFileName = "JPEG_" + timeStamp + "_";
+		String ext = null;
+		if (!temp) {
+			ext = ".jpg";
+		}
+		File image = File.createTempFile(imageFileName, ext, uploadDir);
+		// XXX Save a file: path for use with ACTION_VIEW intents
+		mCurrentPhotoPath = image.getAbsolutePath();
+		return image;
 	}
-	
-	static final int REQUEST_TAKE_PHOTO = 1;
 
 	private void dispatchTakePictureIntent() {
-	    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-	    // Ensure that there's a camera activity to handle the intent
-	    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-	        // Create the File where the photo should go
-	        File photoFile = null;
-	        try {
-	            photoFile = createImageFile();
-	        } catch (IOException ex) {
-	            // Error occurred while creating the File
-	        	ex.printStackTrace();
-	        }
-	        // Continue only if the File was successfully created
-	        if (photoFile != null) {
-	            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-	                    Uri.fromFile(photoFile));
-	            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-	        }
-	    }
+		Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+		// Ensure that there's a camera activity to handle the intent
+		if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+			// Create the File where the photo should go
+			File photoFile = null;
+			try {
+				photoFile = createImageFile(true);
+			} catch (IOException e) {
+				// Error occurred while creating the File
+				e.printStackTrace();
+			}
+			// Continue only if the File was successfully created
+			if (photoFile != null) {
+				takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+						Uri.fromFile(photoFile));
+				startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+			}
+		}
 	}
-}
 
-// CountDownTimer countDownTimer = new CountDownTimer(
-// 4 * 1000, 1000) {
-// public void onTick(long leftTimeInMilliseconds) {
-// long i = leftTimeInMilliseconds / 1000;
-// String j = Long.toString(i);
-// cntr.setText(j);
-// }
-//
-// public void onFinish() {
-// cntr.setText("Get Ready!");
-// Intent intent = new Intent(BrowseActivity.this,
-// in.swifiic.app.andi.photoapp.VoteActivity.class);
-// startActivity(intent);
-// finish();
-// Intent cameraIntent = new
-// Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-// startActivityForResult(cameraIntent , 1337);
-// }
-// }.start();
-// }
-// }
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_TAKE_PHOTO) {
+			if (resultCode == RESULT_OK) {
+				// Bitmap photo = (Bitmap) data.getExtras().get("data");
+				Bitmap photo = BitmapFactory.decodeFile(mCurrentPhotoPath);
+				File tmpFile = new File(mCurrentPhotoPath);
+				// Create the File where the photo should go
+				File photoFile = null;
+				try {
+					photoFile = createImageFile(false);
+				} catch (IOException e1) {
+					// Error occurred while creating the File
+					e1.printStackTrace();
+				}
+				if (photoFile != null) {
+					try {
+						FileOutputStream fOut = new FileOutputStream(photoFile);
+						photo.compress(Bitmap.CompressFormat.JPEG, 25, fOut);
+						fOut.flush();
+						fOut.close();
+						// delete tmp file
+						tmpFile.delete();
+					} catch (IOException e) {
+						tmpFile.delete();
+						// FileOutputStream failed
+						e.printStackTrace();
+						Toast.makeText(this, "Image save compromised",
+								Toast.LENGTH_SHORT).show();
+					}
+				}
+				// Image captured and saved to fileUri specified in the Intent
+				Toast.makeText(
+						this,
+						"Image saved!\nWidth: " + photo.getWidth()
+								+ "\nHeight: " + photo.getHeight(),
+						Toast.LENGTH_LONG).show();
+			} else if (resultCode == RESULT_CANCELED) {
+				// User cancelled the image capture
+			} else {
+				// Image capture failed, advise user
+				Toast.makeText(this, "Oops! Something went wrong.",
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
+}
